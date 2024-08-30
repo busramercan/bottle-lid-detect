b4f9287881c234cb4cc29d9ee21a6b43a0d0985e
bottleliddetect <bottleliddetect@gmail.com>
2024-08-30 20:51:08 +0300

    Test photo is given to the model

--- model_test.py
+++ model_test.py
@@ -1,6 +1,9 @@
 from ultralytics import YOLO
 import cv2
 import math 
+import time
+
+from PIL import Image
 # start webcam
 # cap = cv2.VideoCapture(0)
 # cap.set(3, 640)
@@ -14,23 +17,17 @@ classNames = ["correct", "error", "not-lid"]
 
 print("TEST START")
 
-img = Image.open("test_photo.jpeg")
+start = time.time()
+img = cv2.imread("test_photo3.jpeg")
+end = time.time()
+print("total time to detect: ")
+print((end-start)*1000)
+print("ms")
+
 results = model(img, stream=True)
-
-print(results)
-
-# coordinates
 for r in results:
     boxes = r.boxes
-
     for box in boxes:
-        # bounding box
-        x1, y1, x2, y2 = box.xyxy[0]
-        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
-
-        # put box in cam
-        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-
         # confidence
         confidence = math.ceil((box.conf[0]*100))/100
         print("Confidence --->",confidence)
@@ -39,18 +36,3 @@ for r in results:
         cls = int(box.cls[0])
         print("Class name -->", classNames[cls])
 
-        # object details
-        org = [x1, y1]
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        fontScale = 1
-        color = (255, 0, 0)
-        thickness = 2
-
-        cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
-
-cv2.imshow('Webcam', img)
-#if cv2.waitKey(1) == ord('q'):
-#    break
-
-# cap.release()
-# cv2.destroyAllWindows()
