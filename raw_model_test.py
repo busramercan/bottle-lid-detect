afc55a06669517456e770fdf7dd2f97d977024a0
Busra Mercan <busra.mercan@outlook.com>
2024-09-07 16:20:21 +0300

    Raw model test code is added.

---
+++ raw_model_test.py
@@ -0,0 +1,37 @@
+from ultralytics import YOLO
+import cv2
+import math 
+import time
+
+from PIL import Image
+# start webcam
+# cap = cv2.VideoCapture(0)
+# cap.set(3, 640)
+#cap.set(4, 480)
+
+# model
+model = YOLO("custom_model.pt")
+
+# object classes
+classNames = ["correct", "error", "not-lid"]
+
+print("TEST START")
+img = cv2.imread("test_photo3.jpeg")
+
+start = time.time()
+results = model(img, stream=True)
+end = time.time()
+print("total time to detect: ")
+print((end-start)*1000)
+print("ms")
+
+for r in results:
+    boxes = r.boxes
+    for box in boxes:
+        # confidence
+        confidence = math.ceil((box.conf[0]*100))/100
+        print("Confidence --->",confidence)
+
+        # class name
+        cls = int(box.cls[0])
+        print("Class name -->", classNames[cls])
